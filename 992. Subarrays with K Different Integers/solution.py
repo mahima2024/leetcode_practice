2214b2588fd9a84a8f2ba78b77af968b15d6a014
mahima2024 <157308074+mahima2024@users.noreply.github.com>
2024-03-31 00:37:33 +0530

    Update solution.py

--- 992. Subarrays with K Different Integers/solution.py
+++ 992. Subarrays with K Different Integers/solution.py
@@ -1,1 +1,17 @@
-
+class Solution:
+    def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
+        return self.slidingwindow(nums, k) - self.slidingwindow(nums, k-1)
+    def slidingwindow(self, nums: List[int], m: int) -> int:
+        count = defaultdict(int)
+        res = 0
+        l = 0
+        for r in range(len(nums)):
+            count[nums[r]] += 1
+            while len(count) > m:
+                count[nums[l]] -= 1
+                if count[nums[l]] == 0:
+                    del count[nums[l]]
+                l += 1
+            res += r - l + 1           
+        return res
+        
