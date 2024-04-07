9cb9a16177cf626126eca58b54dad22d7697e0f4
mahima2024 <157308074+mahima2024@users.noreply.github.com>
2024-04-07 23:58:52 +0530

    Update solution.py

--- 678. Valid Parenthesis String/solution.py
+++ 678. Valid Parenthesis String/solution.py
@@ -1,1 +1,17 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        left_max , left_min = 0 , 0
+        for c in s :
+            if c == '(':
+                left_max , left_min = left_max + 1 , left_min + 1
+            elif c == ')':
+                left_max , left_min = left_max - 1 , left_min - 1
+            else:
+                left_max , left_min = left_max + 1 , left_min - 1   
+            if left_max < 0:
+                return False
+            elif left_min < 0:
+                left_min = 0    
+        return left_min == 0        
 
+        
